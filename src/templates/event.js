400abab13e60e000468752b03210cab555ed9053
bartlomiejmusial <bartlomiej.musial.dev@gmail.com>
2022-02-08 22:32:33 +0100

    contact section build

--- src/templates/event.js
+++ src/templates/event.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import Layout from '../layouts/layout';
+import Contact from '../sections/Contact';
 
 const EventTemplate = ({ data }) => {
     const { event } = data;
     return (
-        <Layout additionalClass={['bg-light-gray']}>
-            <div>Event</div>
+        <Layout additionalClass={['bg-white']}>
+            <div className="flex flex-wrap max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+                <Contact
+                    headerText="Do you have more questions?"
+                    nameInputLabel="Name"
+                    emailInputLabel="Email"
+                    messageInputLabel="Message"
+                    buttonLabel="Send"
+                />
+            </div>
         </Layout>
     );
 };

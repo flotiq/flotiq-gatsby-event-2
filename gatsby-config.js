5ea2d0aab08dd56b11692675c4a62cc5714a9385
Kinga Knapik <kinga.knapik@codewave.pl>
2024-04-04 15:59:50 +0200

    bring back sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
             },
         },
         'gatsby-plugin-react-helmet',
-        // 'gatsby-plugin-sitemap',
+        'gatsby-plugin-sitemap',
         {
             resolve: 'gatsby-source-flotiq',
             options: {
